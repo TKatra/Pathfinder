34c8e7ff300003d23934bbe643fc9d789c56d5c5
TKatra <tobiaskatra@hotmail.com>
2016-06-30 23:07:25 +0200

    Added a map factory

---
+++ public/modules/main/services/mapFactory.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+  .module('main')
+  .factory('mapFactory', MapFactory);
+
+  // MapFactory.$inject = [];
+
+  function MapFactory () {
+    return {
+      
+    };
+  }
+})();
