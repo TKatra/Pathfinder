f2dafc256c2162ef877b04a59bb70fb999172f10
TKatra <tobiaskatra@hotmail.com>
2016-07-01 21:04:31 +0200

    Added the pathfinder algorithm, might be working already, need cleaning

--- public/modules/main/controllers/mainController.js
+++ public/modules/main/controllers/mainController.js
@@ -5,29 +5,48 @@
   .module('main')
   .controller('mainController', MainController);
 
-  // MainController.$inject = [];
+  MainController.$inject = ['$rootScope', 'mapFactory'];
 
-  function MainController() {
+  function MainController($rootScope, mapFactory) {
     var vm = this;
     vm.map = [];
     vm.buildMap = buildMap;
-    vm.mapItemClick = mapItemClick
+    vm.mapItemClick = mapItemClick;
+
+    $rootScope.$on('foundPaths', showPaths);
 
     function buildMap() {
       var newMap = [];
+      var newRow;
+
       for (var i = 0; i < vm.rows; i++) {
-        var newRow = [];
+        newRow = [];
         for (var k = 0; k < vm.columns; k++) {
-          newRow.push(randomNumberBetween(1, 10));
+          newRow.push({
+            value: randomNumberBetween(1, 10),
+            fastPath: false,
+            slowPath: false,
+            playerPath: false
+          });
         }
         newMap.push(newRow);
       }
       vm.map = angular.copy(newMap);
+
+      mapFactory.getPaths(vm.map);
+      // mapFactory.getSlowPath(vm.map);
     }
 
-    function mapItemClick(rowIndex, columnIndex) {
-      console.log('rowIndex', rowIndex);
-      console.log('columnIndex', columnIndex);
+    function mapItemClick(item , rowIndex, columnIndex) {
+      console.log('row', rowIndex);
+      console.log('col', columnIndex);
+      
+      item.playerPath = !item.playerPath;
+      console.log(item);
+    }
+
+    function showPaths(event, pathType) {
+      console.log(pathType);
     }
 
     function randomNumberBetween(minNumber, maxNumber) {
--- public/modules/main/services/mapFactory.js
+++ public/modules/main/services/mapFactory.js
@@ -5,11 +5,113 @@
   .module('main')
   .factory('mapFactory', MapFactory);
 
-  // MapFactory.$inject = [];
+  MapFactory.$inject = ['$rootScope'];
 
-  function MapFactory () {
+  function MapFactory ($rootScope) {
     return {
-      
+      getPaths: getPaths
     };
+
+    function getPaths(map) {
+      console.log(map);
+      var allPaths = pathfinder(map);
+      console.log(allPaths);
+      console.log(arrayCleaner(allPaths));
+
+      $rootScope.$emit('foundPaths', 'PARF');
+    }
+
+    function pathfinder(map, row, col, currentPath) {
+      var paths;
+      var nextPath;
+
+      row = row || 0;
+      col = col || 0;
+      currentPath = currentPath || {
+        pathValue: 0,
+        path: []
+      };
+
+      currentPath.pathValue+=map[row][col].value;
+      currentPath.path.push({
+        row: row,
+        col: col
+      });
+
+      // console.log('(' + row +',' + col + '): ' + map[row][col].value);
+      paths = [];
+
+      if (col == 0 && map[row+1]) {
+        // console.log('HOP DOWN');
+        paths.push(pathfinder(map, row+1));
+      }
+
+      if (col == map[row].length-1) {
+        // console.log('STOP');
+        return currentPath;
+      }
+
+      if (map[row-1]) {
+        if (map[row-1][col+1]) {
+          // console.log('up');
+          nextPath = angular.copy(currentPath);
+          paths.push(pathfinder(map, row-1, col+1, nextPath));
+        }
+      }
+      if (map[row][col+1]) {
+        // console.log('forward');
+        nextPath = angular.copy(currentPath);
+        paths.push(pathfinder(map, row, col+1, nextPath));
+      }
+      if (map[row+1]) {
+          if (map[row+1][col+1]) {
+          // console.log('down');
+          nextPath = angular.copy(currentPath);
+          paths.push(pathfinder(map, row+1, col+1, nextPath));
+        }
+      }
+
+
+
+      // console.log(paths);
+      
+      // console.log(arrayCleaner(paths));
+
+      // return arrayCleaner(paths);
+      return paths;
+    }
+
+    function arrayCleaner(uncleanArray, cleanArray, index) {
+      cleanArray = cleanArray || [];
+      index = index || 0;
+
+      // console.log('uncleanArray', uncleanArray);
+      // console.log('cleanArray', cleanArray);
+
+      if (index < uncleanArray.length) {
+        if (Array.isArray(uncleanArray[index])) {
+          arrayCleaner(uncleanArray[index], cleanArray);
+        }
+        else {
+          cleanArray.push(uncleanArray[index]);
+          // return uncleanArray[index];
+        }
+        arrayCleaner(uncleanArray, cleanArray, index+1);
+      }
+
+      // console.log('Return', cleanArray);
+      return cleanArray;
+
+
+
+
+      // var cleanArray = []
+      // for (var i = 0; i < uncleanArray.length; i++) {
+      //   if (uncleanArray[i]) {
+
+      //   }
+      //   else
+      // }
+    }
   }
 })();
