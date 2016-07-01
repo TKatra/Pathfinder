20cb51f106308e379aaf27dcfa7504bb620ea5bc
TKatra <tobiaskatra@hotmail.com>
2016-07-02 00:24:10 +0200

    Now the page shows the paths the computer found. Changed slow to heavy and fast to light.

--- public/modules/main/controllers/mainController.js
+++ public/modules/main/controllers/mainController.js
@@ -13,7 +13,7 @@
     vm.buildMap = buildMap;
     vm.mapItemClick = mapItemClick;
 
-    $rootScope.$on('pathsFound', showPaths);
+    $rootScope.$on('pathsFound', activatePaths);
 
     function buildMap() {
       var newMap = [];
@@ -24,8 +24,8 @@
         for (var k = 0; k < vm.columns; k++) {
           newRow.push({
             value: randomNumberBetween(1, 10),
-            fastPath: false,
-            slowPath: false,
+            lightPath: false,
+            heavyPath: false,
             playerPath: false
           });
         }
@@ -34,7 +34,6 @@
       vm.map = angular.copy(newMap);
 
       mapFactory.getPaths(vm.map);
-      // mapFactory.getSlowPath(vm.map);
     }
 
     function mapItemClick(item , rowIndex, columnIndex) {
@@ -45,8 +44,15 @@
       console.log(item);
     }
 
-    function showPaths(event, pathType) {
-      console.log(pathType);
+    function activatePaths(event, paths) {
+      console.log(paths);
+      vm.lightPath = paths.lightPath;
+      vm.heavyPath = paths.heavyPath;
+
+      for (var i = 0; i < vm.columns; i++) {
+        vm.map[vm.lightPath.path[i].row][vm.lightPath.path[i].col].lightPath = true;
+        vm.map[vm.heavyPath.path[i].row][vm.heavyPath.path[i].col].heavyPath = true;
+      }
     }
 
     function randomNumberBetween(minNumber, maxNumber) {
